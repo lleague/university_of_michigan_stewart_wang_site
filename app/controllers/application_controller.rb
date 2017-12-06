04fbc31835c4bc2d15653dcb70be372e750668a7
Lauren League <lleague@umich.edu>
2017-12-05 20:38:17 -0500

    whitelisted michigan medicine ip addresses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_filter :block_foreign_hosts
+
+  def whitelisted?(ip)
+    return true if ['10.122.16.0/27', '10.16.0.0/12', '10.17.0.0/16', '10.20.0.0/16', '10.21.0.0/16', '10.41.0.0/23', '10.41.128.0/23', '10.41.130.0/23', '10.41.2.0/23', '10.51.0.0/16', '10.60.0.0/16', '10.70.0.0/16', '10.70.29.0/24', '141.214.0.0/16', '141.216.76.0/24', '172.17.0.0/16', '172.20.0.0/16', '172.22.0.0/16', '172.24.0.0/16', '172.25.0.0/16', '10.42.0.0/20', '10.42.128.0/20'].include?(ip)
+    false
+  end
+
+  def block_foreign_hosts
+    return false if whitelisted?(request.remote_ip)
+    redirect_to "https://www.google.com" unless request.remote_ip.start_with?("123.456.789")
+  end
+
   protect_from_forgery with: :exception
   #include SslRequirement
   private
